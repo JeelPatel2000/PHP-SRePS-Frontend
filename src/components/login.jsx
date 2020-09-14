ba3fcca2673ab5860f2dd031d26de2d5b12bc429
parth <patelparth1973@gmail.com>
2020-09-14 13:37:59 +1000

    login started

---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../styles/login.css";
+class Login extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="background">
+        <h1> user Login </h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
