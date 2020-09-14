2ae90e45361adab99b0bd2341cefc76dc380709e
parth <patelparth1973@gmail.com>
2020-09-14 17:35:32 +1000

    design implemented

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 import "../styles/login.css";
+import PersonIcon from "@material-ui/icons/Person";
+import LockIcon from "@material-ui/icons/Lock";
 class Login extends Component {
   state = {};
   render() {
     return (
       <div className="background">
-        <h1> user Login </h1>
+        <div className="login-container">
+          <h2> User Login </h2>
+
+          <form>
+            <div className="username">
+              <span>
+                <PersonIcon />
+              </span>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="password">
+              <span>
+                <LockIcon />
+              </span>
+              <input type="password" placeholder="Password" />
+            </div>
+          </form>
+
+          <button className="loginButton">Login</button>
+        </div>
       </div>
     );
   }
