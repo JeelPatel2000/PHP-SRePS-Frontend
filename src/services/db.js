6d3b3470c45d3ea1ede70cc310ea8a97105a136a
Jeel Patel <jeel.patel1111@gmail.com>
2020-10-07 08:38:03 +1100

    created db uitl for util api requests

---
+++ src/services/db.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export async function getProductsList() {
+  let result = await fetch("http://192.168.1.11:3001/products/list");
+  result = await result.json();
+  await console.log(result);
+  return result;
+}
+
+export async function deleteProduct(_id) {
+  let result = await fetch(`http://192.168.1.11:3001/products/delete/${_id}`);
+  result = await result.json();
+  await console.log(result);
+  return result;
+}
+
+export async function addProduct(product) {
+  let result = await axios({
+    method: "POST",
+    url: "http://192.168.1.11:3001/products/add",
+    data: product,
+  });
+  return await result.data;
+}
