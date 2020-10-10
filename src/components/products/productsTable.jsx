fdc5f91f824bfb4daafa386d0b3901c5e0b5aa99
Jeel Patel <jeel.patel1111@gmail.com>
2020-10-10 14:59:45 +1100

    added edit product modal as child component and few state properties for handling modal

--- src/components/products/productsTable.jsx
+++ src/components/products/productsTable.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import { ProductsContext } from "../../providers/productsProvider";
 import { deleteProduct } from "../../services/db";
+import EditProductModal from "./editProductModal";
 
 class ProductsTable extends Component {
   static contextType = ProductsContext;
+
+  state = {
+    editProductModalShow: false,
+    currentEditProduct: null,
+  };
+
+  handleEditProductModalClose = () => {
+    this.setState({ editProductModalShow: false });
+  };
+
+  handleEditProduct = (product) => {
+    //opening the modal on clicking the edit button and updating the current edit product
+    this.setState({ editProductModalShow: true, currentEditProduct: product });
+  };
 
   handleDeleteProduct = (_id) => {
     const { products, setProducts } = this.context;
     const new_products = products.filter((product) => product._id !== _id);
     setProducts(new_products);
-
     deleteProduct(_id);
   };
 
@@ -21,39 +35,62 @@ class ProductsTable extends Component {
     if (products.length === 0) return <h2>No Products</h2>;
     else {
       return (
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>Product ID</th>
-              <th>Product Name</th>
-              <th>Price</th>
-              <th>Creation Date</th>
-              <th>Stock Count</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => {
-              return (
-                <tr key={product._id}>
-                  <td>{product._id}</td>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.creationDate}</td>
-                  <td>{product.stockCount}</td>
-                  <td>
+        <>
+          <EditProductModal
+            show={this.state.editProductModalShow}
+            handleClose={this.handleEditProductModalClose}
+            product={this.state.currentEditProduct}
+          />
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Product ID</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Creation Date</th>
+                <th>Stock Count</th>
+                {/* <th></th> */}
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => {
+                return (
+                  <tr key={product._id}>
+                    <td>{product._id}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.creationDate}</td>
+                    <td>{product.stockCount}</td>
+                    {/* <td>
                     <button
-                      className="btn btn-danger"
-                      onClick={() => this.handleDeleteProduct(product._id)}
+                      className="btn btn-primary"
+                      onClick={() => this.handleEditProduct(product._id)}
                     >
-                      Delete
+                      Edit
                     </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                  </td> */}
+                    <td>
+                      <button
+                        className="btn btn-primary"
+                        style={{ marginRight: "10px" }}
+                        onClick={() => this.handleEditProduct(product)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => this.handleDeleteProduct(product._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
       );
     }
   }
