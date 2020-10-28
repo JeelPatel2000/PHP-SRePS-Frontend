121d8479af4ebdeec80296d132a272b8ba81fdac
Jeel Patel <jeel.patel1111@gmail.com>
2020-10-29 02:12:54 +1100

    added logout button in navbar

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,32 +1,39 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import React, {Component} from "react";
+import {Button} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <NavLink className="navbar-brand" to="/">
-          People Health Pharmacy
-        </NavLink>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <NavLink className="nav-item nav-link" to="/products">
-              Products
-            </NavLink>
-            <NavLink className="nav-item nav-link" to="/stock">
-              Stock
-            </NavLink>
-            <NavLink className="nav-item nav-link" to="/salesreport">
-              Sales Report
-            </NavLink>
-            <NavLink className="nav-item nav-link" to="/login">
-              Login
-            </NavLink>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+	render() {
+		return (
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<NavLink className="navbar-brand" to="/">
+					People Health Pharmacy
+				</NavLink>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<NavLink className="nav-item nav-link" to="/products">
+							Products
+						</NavLink>
+						<NavLink className="nav-item nav-link" to="/stock">
+							Stock
+						</NavLink>
+						<NavLink className="nav-item nav-link" to="/salesreport">
+							Sales Report
+						</NavLink>
+						<NavLink className="nav-item nav-link" to="/predictsales">
+							Predict Sales
+						</NavLink>
+						<Button
+							className="btn btn-secondary logout_btn"
+							onClick={this.props.logout}
+						>
+							Logout
+						</Button>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
 }
 
 export default Navbar;
