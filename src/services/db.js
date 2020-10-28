da24c265e9b7e037e8c54a257faf70241458422e
Jeel Patel <jeel.patel1111@gmail.com>
2020-10-29 02:18:58 +1100

    refactored url and added salesRecord api

--- src/services/db.js
+++ src/services/db.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
+// axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+
+
+let url = "http://localhost:3001";
+
 export async function getProductsList() {
-  let result = await fetch("http://192.168.1.11:3001/products/list");
+  let result = await fetch(`${url}/products/list`);
   result = await result.json();
   await console.log(result);
   return result;
 }
 
 export async function deleteProduct(_id) {
-  let result = await fetch(`http://192.168.1.11:3001/products/delete/${_id}`);
+  let result = await fetch(`${url}/products/delete/${_id}`);
   result = await result.json();
   await console.log(result);
   return result;
@@ -17,7 +22,7 @@ export async function deleteProduct(_id) {
 export async function addProduct(product) {
   let result = await axios({
     method: "POST",
-    url: "http://192.168.1.11:3001/products/add",
+    url: `${url}/products/add`,
     data: product,
   });
   return await result.data;
@@ -26,9 +31,33 @@ export async function addProduct(product) {
 export async function editProduct(product) {
   let result = await axios({
     method: "POST",
-    url: "http://192.168.1.11:3001/products/edit",
+    url: `${url}/products/edit`,
     data: product,
   });
   await console.log("db call", result.data);
   return await result.data;
+}
+
+export async function addSalesRecord(record){
+  let result = await axios({
+    method: "POST",
+    url: `${url}/salesrecord/add`,
+    data: record,
+  });
+  await console.log("added sales record", result.data);
+  return await result.data;
+}
+
+export async function searchSalesRecord(recordID){
+  let result = await fetch(`${url}/salesrecord/search/${recordID}`);
+  result = await result.json();
+  await console.log(result);
+  return await result;
+}
+
+export async function getAllSalesRecord(){
+  let result = await fetch(`${url}/salesrecord/list`);
+  result = await result.json();
+  await console.log(result);
+  return await result;
 }
